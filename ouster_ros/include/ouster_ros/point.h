351873494304470e5d20d834b3db64a579dc7333
Lovro Markovic <lovro.markovic@fer.hr>
2022-04-05 11:48:51 +0200

    Fix warnings

--- ouster_ros/include/ouster_ros/point.h
+++ ouster_ros/include/ouster_ros/point.h
@@ -10,6 +10,7 @@
 #include <Eigen/Eigen>
 #include <chrono>
 #include <functional>
+#include <cstdint>
 
 #include "ouster/lidar_scan.h"
 
@@ -18,22 +19,22 @@ namespace ouster_ros {
 struct EIGEN_ALIGN16 Point {
     PCL_ADD_POINT4D;
     float intensity;
-    uint32_t t;
-    uint16_t reflectivity;
-    uint8_t ring;
-    uint16_t noise;
-    uint32_t range;
+    std::uint32_t t;
+    std::uint16_t reflectivity;
+    std::uint8_t ring;
+    std::uint16_t noise;
+    std::uint32_t range;
     EIGEN_MAKE_ALIGNED_OPERATOR_NEW
 
     static inline std::function<
         Point(std::ptrdiff_t, std::ptrdiff_t, std::chrono::nanoseconds,
-              std::chrono::nanoseconds, uint32_t, uint16_t, uint16_t, uint16_t)>
+              std::chrono::nanoseconds, std::uint32_t, std::uint16_t, std::uint16_t, std::uint16_t)>
     get_from_pixel(const ouster::XYZLut& xyz_lut, size_t w, size_t h) {
         return [xyz_lut, w, h](std::ptrdiff_t u, std::ptrdiff_t v,
                                std::chrono::nanoseconds ts,
-                               std::chrono::nanoseconds scan_ts, uint32_t range,
-                               uint16_t intensity, uint16_t noise,
-                               uint16_t reflectivity) -> Point {
+                               std::chrono::nanoseconds scan_ts, std::uint32_t range,
+                               std::uint16_t intensity, std::uint16_t noise,
+                               std::uint16_t reflectivity) -> Point {
             const auto xyz = xyz_lut.direction.row(u * w + v) * range +
                              xyz_lut.offset.row(u * w + v);
             return {static_cast<float>(xyz(0)),
@@ -41,11 +42,11 @@ struct EIGEN_ALIGN16 Point {
                     static_cast<float>(xyz(2)),
                     0.0f,
                     static_cast<float>(intensity),
-                    static_cast<uint32_t>((ts - scan_ts).count()),
-                    static_cast<uint16_t>(reflectivity),
-                    static_cast<uint8_t>(u),
-                    static_cast<uint16_t>(noise),
-                    static_cast<uint32_t>(range)};
+                    static_cast<std::uint32_t>((ts - scan_ts).count()),
+                    static_cast<std::uint16_t>(reflectivity),
+                    static_cast<std::uint8_t>(u),
+                    static_cast<std::uint16_t>(noise),
+                    static_cast<std::uint32_t>(range)};
         };
     }
 };
@@ -57,10 +58,10 @@ POINT_CLOUD_REGISTER_POINT_STRUCT(ouster_ros::Point,
     (float, y, y)
     (float, z, z)
     (float, intensity, intensity)
-    (uint32_t, t, t)
-    (uint16_t, reflectivity, reflectivity)
-    (uint8_t, ring, ring)
-    (uint16_t, noise, noise)
-    (uint32_t, range, range)
+    (std::uint32_t, t, t)
+    (std::uint16_t, reflectivity, reflectivity)
+    (std::uint8_t, ring, ring)
+    (std::int16_t, noise, noise)
+    (std::uint32_t, range, range)
 )
 // clang-format on
